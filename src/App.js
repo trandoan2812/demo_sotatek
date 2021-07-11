5b506ae4a5c701cf8651bf1916ed6d4bdcb8000d
Doan Tran <doan.tran@ntq-solution.com.vn>
2021-07-11 23:06:08 +0700

    get position when click maps

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import './App.css';
+import "./App.css";
+import { CaretLeftOutlined, CaretRightOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import $ from "jquery";
+import { notification } from "antd";
+import "antd/dist/antd.css";
 
 function App() {
+  const [isMoveDirection, setIsMoveDirection] = useState("");
+
+  const onMoveDirection = (direction) => {
+    setIsMoveDirection(direction);
+  };
+
+  const onMoveStreet = (e) => {
+    notification.info({
+      message: "Position",
+      description: `Top: ${e.pageY}, Left: ${e.pageX}`,
+      placement: "bottomLeft",
+    });
+  };
+
   return (
     <div className="App">
-      <div className="bgImage" ></div>
+      <div
+        onClick={onMoveStreet}
+        className={`bg-image ${isMoveDirection === "left" && "bg-move-left"} ${
+          isMoveDirection === "right" && "bg-move-right"
+        }`}
+      >
+        <CaretLeftOutlined
+          onClick={() => onMoveDirection("left")}
+          className="icon-direction"
+        />
+        <CaretRightOutlined
+          onClick={() => onMoveDirection("right")}
+          className="icon-direction icon-direction--right"
+        />
+      </div>
     </div>
   );
 }
