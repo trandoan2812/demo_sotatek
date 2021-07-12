5e02442c57f4be67ebc7f26b959719fd3d32a9c6
Doan Tran <doan.tran@ntq-solution.com.vn>
2021-07-13 00:01:05 +0700

    edit css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
-import { CaretLeftOutlined, CaretRightOutlined, PushpinFilled } from "@ant-design/icons";
+import {
+  CaretLeftOutlined,
+  CaretRightOutlined,
+  PushpinFilled,
+} from "@ant-design/icons";
 import { useState } from "react";
 import $ from "jquery";
 import "antd/dist/antd.css";
@@ -8,8 +12,14 @@ function App() {
   const [isMoveDirection, setIsMoveDirection] = useState("");
   const [positionPin, setPositionPin] = useState(null);
 
-  const onMoveDirection = (direction) => {
-    setIsMoveDirection(direction);
+  const onMoveDirectionLeft = (e) => {
+    e.stopPropagation();
+    setIsMoveDirection("left");
+  };
+
+  const onMoveDirectionRight = (e) => {
+    e.stopPropagation();
+    setIsMoveDirection("right");
   };
 
   const onMoveStreet = (e) => {
@@ -17,10 +27,11 @@ function App() {
     const left = e.pageX;
     setPositionPin({
       top,
-      left
-    })
+      left,
+    });
   };
 
+  console.log("positionPin: ", positionPin);
   return (
     <div className="App">
       <div
@@ -30,14 +41,31 @@ function App() {
         }`}
       >
         <CaretLeftOutlined
-          onClick={() => onMoveDirection("left")}
+          onClick={onMoveDirectionLeft}
           className="icon-direction"
         />
         <CaretRightOutlined
-          onClick={() => onMoveDirection("right")}
+          onClick={onMoveDirectionRight}
           className="icon-direction icon-direction--right"
         />
-        <PushpinFilled style={{ display: positionPin ? 'block' : 'none', top: positionPin ? positionPin.top : '0', left: positionPin ? positionPin.left : '0'}} className="icon-pin" />
+
+        {positionPin && (
+          <>
+            <PushpinFilled
+              style={{
+                display: positionPin ? "block" : "none",
+                top: positionPin ? positionPin.top : "0",
+                left: positionPin ? positionPin.left : "0",
+              }}
+              className="icon-pin"
+            />
+            <div className="pin-information">
+              <div>position: </div>
+              <div>top: {positionPin.top}</div>
+              <div>left: {positionPin.left}</div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
